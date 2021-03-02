d386a267610b947af3ac0ab336f17fe9bab1804e
Konrad Łukasz Polański <k.polanski@stud.elka.pw.edu.pl>
2021-03-02 12:37:37 +0100

    Pierwszy commit

---
+++ aplikacja.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, session
+from flask_session import Session
+
+app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+notes = []
+
+@app.route("/gra", methods=["GET", "POST"])
+def gra():
+    if request.method == "POST":
+        note = request.form.get("note")
+        notes.append(note)
+    return render_template("Gra.html", notes=notes)
+
+@app.route("/ruchy")
+def ruchy():
+    return render_template("Ruchy.html")
+
+@app.route("/zasady")
+def zasady():
+    return render_template("Zasady.html")
+
+@app.route("/wstep")
+def wstep():
+    return render_template("Wstep.html")
